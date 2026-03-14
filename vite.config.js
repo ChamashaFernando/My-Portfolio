729ea88489ff180ff0d2aa93f0ee6ed852f0f9b5
ChamashaFernando <chamashanethmi81@gmail.com>
2026-03-14 15:24:16 +0530

    fix vite base for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,3 @@
-// import { defineConfig, loadEnv } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-
-// // https://vite.dev/config/
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), "");
-//   return {
-//     base: `/${env.VITE_REPO_NAME}/`,
-//     plugins: [react(), tailwindcss()],
-//     server: {
-//       open: true,
-//       host: false, // Allow access from network devices
-//       proxy: {
-//         "/api": {
-//           target: "http://localhost:5000",
-//           changeOrigin: true,
-//           secure: true,
-//         },
-//       },
-//     },
-//   };
-// });
 
 
 
@@ -34,7 +11,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     // ✅ IMPORTANT for Vercel (fixes 404 white page issue)
-    base: "./",
+    base: "/",
 
     plugins: [react(), tailwindcss()],
 
