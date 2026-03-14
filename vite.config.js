e2ce9a677f8a4ce2ac3abededfdec39224decd92
ChamashaFernando <chamashanethmi81@gmail.com>
2026-03-14 14:48:49 +0530

    fix: add base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,29 @@
+// import { defineConfig, loadEnv } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
+//   return {
+//     base: `/${env.VITE_REPO_NAME}/`,
+//     plugins: [react(), tailwindcss()],
+//     server: {
+//       open: true,
+//       host: false, // Allow access from network devices
+//       proxy: {
+//         "/api": {
+//           target: "http://localhost:5000",
+//           changeOrigin: true,
+//           secure: true,
+//         },
+//       },
+//     },
+//   };
+// });
+
+
+
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -5,17 +31,21 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+    // ✅ IMPORTANT for Vercel (fixes 404 white page issue)
+    base: "./",
+
     plugins: [react(), tailwindcss()],
+
     server: {
       open: true,
-      host: false, // Allow access from network devices
+      host: true, // allow network access
       proxy: {
         "/api": {
           target: "http://localhost:5000",
           changeOrigin: true,
-          secure: true,
+          secure: false,
         },
       },
     },
